fbd53d381ea4e61c15f7a398ead7434dd034bc77
ShinDongJun <wq0212@naver.com>
2022-11-13 18:37:48 +0900

    feat: 컴포넌트에 props로 language 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import "./App.css";
 
-import { useDispatch, useSelector } from "react-redux";
-import { toEnglish, toKorean } from "./store/modules/language";
+import { useSelector } from "react-redux";
+
 import { RootState } from "./store/modules";
 
 import Header from "./components/header";
 
 function App() {
-  const dispatch = useDispatch();
   const language = useSelector(
     (state: RootState) => state.setLanguage.language
   );
   return (
     <div className="App">
       <p>{language}</p>
-      <button onClick={() => dispatch(toKorean())}> to korean</button>
-      <button onClick={() => dispatch(toEnglish())}> to english</button>
-      <Header></Header>
+      <Header language={language}></Header>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toEnglish, toKorean } from "../store/modules/language";
 
-function Header() {
+type HeaderProps = {
+  language: string;
+};
+function Header(props: HeaderProps) {
+  const dispatch = useDispatch();
+  const language = props.language;
   return (
     <div>
-      <h3>이름</h3>
+      <button onClick={() => dispatch(toKorean())}> to korean</button>
+      <button onClick={() => dispatch(toEnglish())}> to english</button>
+      {language === "ENG" ? <h3>Name</h3> : <h3>이름</h3>}
     </div>
   );
 }
