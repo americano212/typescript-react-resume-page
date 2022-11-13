92e41c6ffc35c43da9bb78266fc539f58bf97221
ShinDongJun <wq0212@naver.com>
2022-11-14 00:19:07 +0900

    feat: header 정보 넣고 만들기

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { toEnglish, toKorean } from "../store/modules/language";
+import styled from "styled-components";
 
 type HeaderProps = {
   language: string;
 };
+
 function Header(props: HeaderProps) {
   const dispatch = useDispatch();
   const language = props.language;
@@ -12,9 +14,20 @@ function Header(props: HeaderProps) {
     <div>
       <button onClick={() => dispatch(toKorean())}> to korean</button>
       <button onClick={() => dispatch(toEnglish())}> to english</button>
-      {language === "ENG" ? <h3>Name</h3> : <h3>이름</h3>}
+      <NameText>{language === "ENG" ? "Dongjun Shin" : "신동준"}</NameText>
+      <SmallText>me@dongjun.me</SmallText>
+      <SmallText>Seoul, Republic of Korea</SmallText>
     </div>
   );
 }
+
+const NameText = styled.div`
+  font-size: large;
+  font-weight: bold;
+`;
+
+const SmallText = styled.div`
+  font-size: small;
+`;
 
 export default Header;
