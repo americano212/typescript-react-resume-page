aec9d38fef4a799eb90b74fa0e48585a65b064ad
ShinDongJun <wq0212@naver.com>
2022-11-17 03:27:14 +0900

    feat: awards 완성

--- src/components/atoms/Contents.tsx
+++ src/components/atoms/Contents.tsx
@@ -3,14 +3,34 @@ import styled from "styled-components";
 
 type ContentsTextProps = {
   children: string;
+  bold?: string;
 };
 
 function Contents(props: ContentsTextProps) {
-  return <ContentsText>{props.children}</ContentsText>;
+  const bold = props.bold;
+  return (
+    <ContentsDiv>
+      <ContentsText>{props.children}</ContentsText>
+      {bold !== undefined ? (
+        <ContentsTextBold>{bold}</ContentsTextBold>
+      ) : (
+        <ContentsTextBold></ContentsTextBold>
+      )}
+    </ContentsDiv>
+  );
 }
 
-const ContentsText = styled.div`
+const ContentsDiv = styled.div`
   float: left;
+`;
+
+const ContentsText = styled.span`
   font-size: small;
 `;
+
+const ContentsTextBold = styled.span`
+  font-size: small;
+  font-weight: bold;
+`;
+
 export default Contents;
