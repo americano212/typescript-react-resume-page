40b7e36eb431ac34f130d69aed2038dfaeeeab28
ShinDongJun <wq0212@naver.com>
2022-11-17 03:33:48 +0900

    feat: 자격증 추가

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 import { RootState } from "../store/modules";
+import ContentDate from "./atoms/ContentDate";
+import Contents from "./atoms/Contents";
 
 import TitleBox from "./atoms/TitleBox";
 
@@ -12,9 +15,21 @@ function Certificate() {
   return (
     <div>
       <TitleBox title={"CERTIFICATE"} />
-      {language}
+      <ULSmallMargin>
+        <li>
+          <Contents>
+            {language === "ENG"
+              ? "Internet security expert, Grade II"
+              : "인터넷보안전문가 2급"}
+          </Contents>
+          <ContentDate>(2021.12)</ContentDate>
+        </li>
+      </ULSmallMargin>
     </div>
   );
 }
-
+const ULSmallMargin = styled.ul`
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
 export default Certificate;
