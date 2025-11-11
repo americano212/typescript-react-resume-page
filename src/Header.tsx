bd50170729469430314fc5b4b2a88f20193601af
Dongjun Shin <wq0212@naver.com>
2025-11-11 21:26:14 +0900

    feat: update name

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,28 @@
-import { HeaderLeftBox, HeaderRightBox, HeaderWrapper, NameBox, TextBox } from "./styles/header";
+import {
+  HeaderLeftBox,
+  HeaderRightBox,
+  HeaderWrapper,
+  NameBox,
+  TextBox,
+} from "./styles/header";
 import Flag from "./components/atoms/Flag";
 import { Language } from "./types/language.enum";
 import { useSelector } from "react-redux";
 import { RootState } from "./store/modules";
 
 export default function Header() {
-  const language: Language = useSelector((state: RootState) => state.setLanguage.language);
+  const language: Language = useSelector(
+    (state: RootState) => state.setLanguage.language
+  );
 
   return (
     <HeaderWrapper>
       <HeaderLeftBox>
-        <NameBox>{language === Language.eng ? "Dongjun Shin" : "신동준"}</NameBox>
+        <NameBox>
+          {language === Language.eng
+            ? "Dongjun Shin(신동준)"
+            : "신동준(Dongjun Shin)"}
+        </NameBox>
         <TextBox>me@dongjun.me</TextBox>
         <TextBox>Seoul, Republic of Korea</TextBox>
       </HeaderLeftBox>
