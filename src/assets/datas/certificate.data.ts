ba3594823276003c0f78c51262754bcdaa33d0c3
Dongjun Shin <wq0212@naver.com>
2025-12-03 11:14:27 +0900

    docs: update TOPCIT score

--- src/assets/datas/certificate.data.ts
+++ src/assets/datas/certificate.data.ts
@@ -1,45 +1,45 @@
 import { Content } from "../../types/content.interface";
 
 export const certificateData: Content[] = [
-  { 
+  {
     isDisplay: true,
     details: [
       {
-        enText: "Linux Master, Grade II" ,
+        enText: "TOPCIT (Grade IV, 693 points)",
+        krText: "TOPCIT (수준4, 693점)",
+        date: "(2025.12)",
+        isDisplay: true,
+      },
+      {
+        enText: "Linux Master, Grade II",
         krText: "리눅스마스터 2급",
         date: "(2024.12)",
-        isDisplay: true
+        isDisplay: true,
       },
       {
-        enText: "AWS Certified Solutions Architect - Associate(AWS SAA)" ,
+        enText: "AWS Certified Solutions Architect - Associate(AWS SAA)",
         krText: "AWS Certified Solutions Architect - Associate(AWS SAA)",
         date: "(2024.11)",
-        isDisplay: true
+        isDisplay: true,
       },
       {
-        enText: "SQL Developer(SQLD)" ,
+        enText: "SQL Developer(SQLD)",
         krText: "SQL 개발자(SQLD)",
         date: "(2023.10)",
-        isDisplay: true
+        isDisplay: true,
       },
       {
-        enText: "TOPCIT (Grade III, 535 points)" ,
-        krText: "TOPCIT (수준3, 535점)",
-        date: "(2022.12)",
-        isDisplay: true
-      },
-      {
-        enText: "Industrial Engineer Information Processing" ,
+        enText: "Industrial Engineer Information Processing",
         krText: "정보처리산업기사",
         date: "(2022.11)",
-        isDisplay: true
+        isDisplay: true,
       },
       {
-        enText: "Internet security expert, Grade II" ,
+        enText: "Internet security expert, Grade II",
         krText: "인터넷보안전문가 2급",
         date: "(2021.12)",
-        isDisplay: true
+        isDisplay: true,
       },
-    ]
+    ],
   },
-]
+];
